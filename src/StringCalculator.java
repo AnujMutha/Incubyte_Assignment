f0e5f33d4f2a6bf4b1c93baaf4d8f138ba061233
A N U J  M U T H A <mutha.am16@gmail.com>
2022-09-07 17:41:45 +0530

    Commit - 2
    String Tokenization Steps

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,3 +1,5 @@
+import com.sun.deploy.util.StringUtils;
+
 public class StringCalculator {
 
     int add(String numbers){
@@ -5,7 +7,13 @@ public class StringCalculator {
         int finalAnswer = 0;
         //Main Code Starts Here
 
-        
+        String[] tokens;
+        if (numbers.startsWith("//")) {
+            String delimiter = numbers.substring(2, 3);
+            tokens = StringUtils.splitString(numbers.substring(4), delimiter);
+        } else {
+            tokens = StringUtils.splitString(numbers, ",\n");
+        }
 
         //End
         return finalAnswer;
