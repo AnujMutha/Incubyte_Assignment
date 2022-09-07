7113b643e2b69245ce1778d809a1efd6e3f9ebf6
A N U J  M U T H A <mutha.am16@gmail.com>
2022-09-07 17:56:49 +0530

    Commit - 5
    Added code block to identify whether any element is negative, if any element is found to be negative then it is added to a list and at last an exception is raised that displays the negative number in the exception message

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         StringCalculator sc = new StringCalculator();
-        System.out.println(sc.add("1,2,3,4"));
+        System.out.println(sc.add("1,2,3,4,a"));
     }
 }
--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,10 +1,15 @@
 import com.sun.deploy.util.StringUtils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringCalculator {
 
     int add(String numbers){
 
         int finalAnswer = 0;
+        boolean negFlag = false;
+        List<Integer> negNumbers = new ArrayList();
         //Main Code Starts Here
 
         String[] tokens;
@@ -15,11 +20,16 @@ public class StringCalculator {
             tokens = StringUtils.splitString(numbers, ",\n");
         }
         for(int i = 0; i < tokens.length; ++i) {
-            if (tokens[i].matches(".*\\d.*")) {
+            if (tokens[i].matches(".*-\\d.*")) {
+                negNumbers.add(Integer.parseInt(tokens[i]));
+                negFlag = true;
+            }
+
+            if (tokens[i].matches(".*\\d.*") && !negFlag) {
                 if (Integer.parseInt(tokens[i]) <= 1000) {
                     finalAnswer += Integer.parseInt(tokens[i]);
                 }
-            }else if (tokens[i].matches(".*[a-zA-Z].*")) {
+            } else if (tokens[i].matches(".*[a-zA-Z].*") && !negFlag) {
                 char c = tokens[i].toLowerCase().charAt(0);
                 int alphabetPosition = c - 96;
                 finalAnswer += alphabetPosition;
