11128a45c306413f8fee9bb6ab485adcfe2bb7f0
A N U J  M U T H A <mutha.am16@gmail.com>
2022-09-07 17:40:01 +0530

    Commit - 1
    Class Template Setup

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        StringCalculator sc = new StringCalculator();
+        System.out.println(sc.add("1,2,3,4"));
+    }
+}
---
+++ src/StringCalculator.java
@@ -0,0 +1,14 @@
+public class StringCalculator {
+
+    int add(String numbers){
+
+        int finalAnswer = 0;
+        //Main Code Starts Here
+
+        
+
+        //End
+        return finalAnswer;
+    }
+
+}
