d7cc4ee48f7512bff25699ced6d4c597939e65d9
A N U J  M U T H A <mutha.am16@gmail.com>
2022-09-07 18:18:23 +0530

    Commit - 6
    Added comments in order to understand the code

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
+        //Instantiating the StringCalculator class
         StringCalculator sc = new StringCalculator();
+        //Calling add method by passing string as argumnet
         System.out.println(sc.add("-1,-2,3,-4,a"));
     }
 }
--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -7,34 +7,63 @@ public class StringCalculator {
 
     int add(String numbers){
 
+        //Defining the variables and datastructures required
         int finalAnswer = 0;
         boolean negFlag = false;
-        List<Integer> negNumbers = new ArrayList();
-        //Main Code Starts Here
 
+        // To store the negative elements from the string
+        List<Integer> negNumbers = new ArrayList();
+
+        //Main Code Starts Here
+        // Array of string to store the elements of string as tokens in the array
         String[] tokens;
+
+        //Checking whether the string starts with double leading slash,
+        //if yes then split the string based on the given delimiter and if no then splitting the string based on ',' or '\n'
         if (numbers.startsWith("//")) {
             String delimiter = numbers.substring(2, 3);
             tokens = StringUtils.splitString(numbers.substring(4), delimiter);
         } else {
             tokens = StringUtils.splitString(numbers, ",\n");
         }
+
+        //Looping through all the tokens (elements of the string)
         for(int i = 0; i < tokens.length; ++i) {
+
+            //Checking whether the token is a negative number or not,
+            //if yes then adding it to the list of negative numbers amd setting the flag as true hence an exception can be raised`
             if (tokens[i].matches(".*-\\d.*")) {
                 negNumbers.add(Integer.parseInt(tokens[i]));
                 negFlag = true;
             }
 
+            //Checking whether the token is a number or not if yes then adding it to the resultant variable
             if (tokens[i].matches(".*\\d.*") && !negFlag) {
                 if (Integer.parseInt(tokens[i]) <= 1000) {
                     finalAnswer += Integer.parseInt(tokens[i]);
                 }
+
+            //Checking whether the token is alphabet
+            //if yes then identifying the standard occurring positions of the alphabet and then adding position in the resultant sum
             } else if (tokens[i].matches(".*[a-zA-Z].*") && !negFlag) {
+
+                //Converting the current char in lowercase in order to prevent wrong calculations
                 char c = tokens[i].toLowerCase().charAt(0);
+                //To get the standard position of the alphabet in the english alphabet table,
+                //we subtract 96 from the ascii value of current char in order to get the position of the alphabet
+                /* The following intuition works because
+                *  the lowercase a - z starts from 97 and if we subtract 96 from we get position
+                * For Example : a = 97 - 96 = 1
+                *               b = 98 - 96 = 2
+                * */
                 int alphabetPosition = c - 96;
+
+                //Finally, adding the position into resultant variable
                 finalAnswer += alphabetPosition;
             }
         }
+
+        //Checking whether negative element flag is set, if yes then raising exception
         if (negFlag) {
             throw new IllegalArgumentException("negatives not allowed " + negNumbers);
         }
