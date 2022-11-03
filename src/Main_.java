076eb4d4aae23a3bc5f1c4ae78060490d3d8b8c6
A N U J  M U T H A <mutha.am16@gmail.com>
2022-11-03 13:28:35 +0530

    .

---
+++ src/Main_.java
@@ -0,0 +1,25 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //Instantiating the StringCalculator class
+        StringCalculator sc = new StringCalculator();
+        //Calling add method by passing string as argument
+        System.out.println(sc.add("1,2,3,1001,1000,1,2"));
+    }
+}
+
+/*
+* Test Cases :
+* 1 : ("")
+* 2 : ("1")
+* 3 : ("1,2,3,4")
+* 4 : ("a,b,c,d")
+* 5 : ("1,2,3,4,a,b,c,d")
+* 6 : ("1,2,3\n4,5,6\nA\nb")
+* 7 : ("//:\n1:2:3:4:5:a:b")
+* 8 : ("1,2,3,1001,1000,1,2")
+<<<<<<< HEAD
+*/
+=======
+*/
+>>>>>>> 080eca7ccb4709987e78d0e486f9640aa5f418ea
